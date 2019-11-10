905d5e92a9112e386609916c25c25e4483508239
Halis Kara <bllhlskr@gmail.com>
2019-11-10 11:30:03 +0300

    Made by Halis Kara.

--- 3D Projects/3d FPS Game/Assets/scripts/AmmoType.cs
+++
@@ -1,4 +0,0 @@
-﻿public enum AmmoType
-{
-    Bullets,Shells,Rockets,Bombs
-}
--- 3D Projects/RPG 2/Assets/Scripts/Core/IAction.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-namespace RPG.Core
-{
-    public interface IAction
-    {
-        void Cancel();
-    }
-}
